73018e8309e95b651eb5cc8b7dc0266e1f61730f
arhamhundia007 <hundia@purdue.edu>
2020-08-13 15:53:20 +0530

    finally

---
+++ project 2/src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+//        RedBlackBST <Integer, Integer> test2 = new RedBlackBST<>();
+//        Scanner s = new Scanner(System.in);
+//        while (s.hasNextLine()) {
+//            String[] input = s.nextLine().split(" ");
+//
+//            for (String x : input) {
+//                System.out.print(x + " ");
+//            }
+//
+//            System.out.println();
+//            switch (input[0]) {
+//                case "insert":
+//                    Integer key = Integer.parseInt(input[1]);
+//                    Integer val = Integer.parseInt(input[2]);
+//                    test.insert(key, val);
+//                    printTree(test.root);
+//                    System.out.println();
+//                    break;
+//    }
+    }
+}
